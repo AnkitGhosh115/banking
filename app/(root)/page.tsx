edaa6d31fb7b812746bccd4dc88144a3b46c81fb
unknown <ankitghosh115@gmail.com>
2024-08-21 12:37:58 +0530

    Implement complete layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,12 @@
 import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar';
 import TotalBalanceBox from '@/components/TotalBalanceBox';
+import Image from "next/image";
+import Link from 'next/link'
 import React from 'react'
 
 const Home = () => {
-    const loggedIn = { firstName: 'Ankit'};
+    const loggedIn = { firstName: 'Ankit', lastName: 'Ghosh', email: 'ankitghosh1151@gmail.com'};
 
   return (
     <section className="home">
@@ -22,7 +25,15 @@ const Home = () => {
                     totalCurrentBalance={4250.65}
                 />
             </header>
+
+            RECENT TRANSACTIONS
         </div>
+
+        <RightSidebar 
+            user={loggedIn}
+            transactions={[]}
+            banks={[{currentBalance: 1330.50}, {currentBalance: 235.65}]}
+        />
     </section>
   )
 }
